bf99d50f879247aff1244c2950b5919b34a9a072
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2023-07-11 19:40:47 +0200

    pure functions

--- Debug/debug.js
+++ Debug/debug.js
@@ -7,7 +7,7 @@ export const GSLDebug = new GSLClass('GSL/Debug');
 GSLDebug.addFunction('print', function (x) {
     GSLDebug.debug(x.value);
 }, {
-    pure: true,
+    pure: false,
     static: true,
     inputs: {value: {type: 'string', name: 'value'}}
 });
